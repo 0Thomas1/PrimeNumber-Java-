e22fab590b55edc7ca3d27a563ac65320729b085
0Thomas1 <99365833+0Thomas1@users.noreply.github.com>
2022-06-22 10:18:55 -0700

    implemented findPrimeFactors

--- primeNumberAlgo.java
+++ primeNumberAlgo.java
@@ -17,6 +17,7 @@ public class primeNumberAlgo {
         int high = in.nextInt();
         System.out.println(findPrimeInRange(low, high));
         System.out.println(findTwinPrimeInRange(low, high));
+        System.out.println(findPrimeFactors(77));
     }
     public static Boolean checkPrime(int num){
         //check if the input no. is prime or not, and return boolean expression
@@ -31,7 +32,7 @@ public class primeNumberAlgo {
     }
     public static List<Integer> findPrimeInRange(int lower, int higher){
         //find all the prime no. in the range return a list
-        List<Integer> primeLst = new ArrayList<Integer>();
+        List<Integer> primeLst = new ArrayList<>();
         for (int i = lower; i <= higher; i++) {
             if (checkPrime(i)) {
                 primeLst.add(i);
@@ -42,17 +43,33 @@ public class primeNumberAlgo {
     public static List<Integer> findTwinPrimeInRange (int lower, int higher){
         // input range, return a list with all twin prime
         List<Integer> primeLst = findPrimeInRange(lower, higher);
-        List<Integer> twinPrimeLst = new ArrayList<Integer>();
+        List<Integer> twinPrimeLst = new ArrayList<>();
 
         // iterate the  prime list then put twin primes into the twin prime list
         for (int i = 0; i < primeLst.size()-1; i++){
-            if (primeLst.get(i+1) - primeLst.get(i) == 2){
-                twinPrimeLst.add(primeLst.get(i));
-                twinPrimeLst.add(primeLst.get(i+1));
+            int bigP = primeLst.get(i+1);
+            int smallP = primeLst.get(i);
+            if (bigP - smallP == 2){
+                twinPrimeLst.add(smallP);
+                twinPrimeLst.add(bigP);
             }
         }
         return twinPrimeLst;
-
-
+    }
+    public static List<Integer> findPrimeFactors(int number){
+        List<Integer> primeLst = findPrimeInRange(0, number);
+        List<Integer> primeFactorLst = new ArrayList<>();
+        for(int i = 0; i< primeLst.size(); i++){
+            for (int j = i+1; j < primeLst.size()&& primeFactorLst.size()==0;){
+                if (primeLst.get(i) * primeLst.get(j) != number) {
+                    j+=1;
+                }
+                else{
+                        primeFactorLst.add(primeLst.get(i));
+                        primeFactorLst.add(primeLst.get(j));
+                }
+            }
+        }
+        return primeFactorLst;
     }
 }
